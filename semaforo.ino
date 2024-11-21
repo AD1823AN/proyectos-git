dce88418915df135d4acf21fc353a671b799904b
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-20 22:13:11 -0600

    Add files via upload

--- semaforo.ino
+++ semaforo.ino
@@ -1,69 +1,124 @@
-int Rojo1 = 5;
-int Ambar1 = 6;
-int Verde1 = 7;
-int Rojo2 = 8;
-int Ambar2 = 9;
+const int btn1 = 9;
+const int btn2 = 10;
+const int R1 = 4;
+const int am1 = 3;
+const int ver1 = 2;
 
-int Verde2 = 10;
+const int R2 = 7;
+const int am2 = 6;
+const int ver2 = 5;
+
+const int T1 = 1000;
+const int TP1 = 500;
 
 void setup() {
-  pinMode(Rojo1,OUTPUT); 
-  pinMode(Ambar1,OUTPUT);
-  pinMode(Verde1,OUTPUT);
-  pinMode(Rojo2,OUTPUT);
-  pinMode(Ambar2,OUTPUT);
-  pinMode(Verde2,OUTPUT);
+  pinMode(btn1, INPUT);
+  pinMode(btn2, INPUT);
+
+  pinMode(R1, OUTPUT);
+  pinMode(am1, OUTPUT);
+  pinMode(ver1, OUTPUT);
+
+  pinMode(R2, OUTPUT);
+  pinMode(am2, OUTPUT);
+  pinMode(ver2, OUTPUT);
 }
 
 void loop() {
-  //Ciclo semaforo2
-  digitalWrite(Rojo1,HIGH);
-  digitalWrite(Ambar1,LOW);
-  digitalWrite(Verde1,LOW);
-  
-  digitalWrite(Rojo2,LOW);
-  digitalWrite(Ambar2,LOW);
-  digitalWrite(Verde2,HIGH);
-  delay(3000);
+  int Encendido = digitalRead(btn1);
+  int Apagado = digitalRead(btn2);
 
-  //Parpadeo semaforo2
-  digitalWrite(Verde2,LOW);
-  delay(1000);
-  digitalWrite(Verde2,HIGH);
-  delay(1000);
-  digitalWrite(Verde2,LOW);
-  delay(1000);
-  digitalWrite(Verde2,HIGH);
-  delay(1000);
-  
-  //Cambio semaforo2
-  digitalWrite(Ambar2,HIGH);
-  digitalWrite(Verde2,LOW);
-  delay(1000);
-  
-  //Ciclo semaforo1
-  digitalWrite(Rojo1,LOW);
-  digitalWrite(Ambar1,LOW);
-  digitalWrite(Verde1,HIGH);
-  
-  digitalWrite(Rojo2,HIGH);
-  digitalWrite(Ambar2,LOW);
-  digitalWrite(Verde2,LOW);
-  delay(3000);
+  if (Encendido == HIGH) {
+    while (true) { // Ciclo infinito
+      if (digitalRead(btn2) == HIGH) { // Verifica si se presiona btn2
+        apagarTodo();
+        break; // Sale del ciclo infinito
+      }
 
-  //Parpadeo semaforo1
-  digitalWrite(Verde1,LOW);
-  delay(1000);
-  digitalWrite(Verde1,HIGH);
-  delay(1000);
-  digitalWrite(Verde1,LOW);
-  delay(1000);
-  digitalWrite(Verde1,HIGH);
-  delay(1000);
+      // LED ROJO 1
+      digitalWrite(R1, HIGH);
+      digitalWrite(R2, LOW);
+      digitalWrite(am1, LOW);
+      digitalWrite(am2, LOW);
+      digitalWrite(ver1, LOW);
+      digitalWrite(ver2, HIGH);
+      delay(T1);
+
+      if (digitalRead(btn2) == HIGH) {
+        apagarTodo();
+        break;
+      }
+
+      // LED Amarillo 1
+      digitalWrite(R1, HIGH);
+      digitalWrite(R2, LOW);
+      digitalWrite(am1, LOW);
+      digitalWrite(am2, HIGH);
+      digitalWrite(ver1, LOW);
+      digitalWrite(ver2, LOW);
+      delay(TP1);
+
+      if (digitalRead(btn2) == HIGH) {
+        apagarTodo();
+        break;
+      }
+
+      // LED VERDE 1
+      digitalWrite(R1, LOW);
+      digitalWrite(R2, HIGH);
+      digitalWrite(am1, LOW);
+      digitalWrite(am2, LOW);
+      digitalWrite(ver1, HIGH);
+      digitalWrite(ver2, LOW);
+      delay(T1);
+
+      if (digitalRead(btn2) == HIGH) {
+        apagarTodo();
+        break;
+      }
+
+      // LED Amarillo 2
+      digitalWrite(R1, LOW);
+      digitalWrite(R2, HIGH);
+      digitalWrite(am1, HIGH);
+      digitalWrite(am2, LOW);
+      digitalWrite(ver1, LOW);
+      digitalWrite(ver2, LOW);
+      delay(TP1);
+
+      if (digitalRead(btn2) == HIGH) {
+        apagarTodo();
+        break;
+      }
+
+      // LED VERDE 2
+      digitalWrite(R1, HIGH);
+      digitalWrite(R2, LOW);
+      digitalWrite(am1, LOW);
+      digitalWrite(am2, LOW);
+      digitalWrite(ver1, LOW);
+      digitalWrite(ver2, HIGH);
+      delay(T1);
+
+      if (digitalRead(btn2) == HIGH) {
+        apagarTodo();
+        break;
+      }
+    }
+  }
   
-  //Cambio semaforo1
-  digitalWrite(Ambar1,HIGH);
-  digitalWrite(Verde1,LOW);
-  delay(1000);
-  
+  if (Apagado == HIGH) {
+    apagarTodo();
+  }
 }
+
+void apagarTodo() {
+  digitalWrite(R1, LOW);
+  digitalWrite(R2, LOW);
+  digitalWrite(am1, LOW);
+  digitalWrite(am2, LOW);
+  digitalWrite(ver1, LOW);
+  digitalWrite(ver2, LOW);
+}
+
+
