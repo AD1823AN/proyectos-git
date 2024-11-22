4a77df4d7291ec379588aa262d56466e6aeb12a5
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-21 22:45:43 -0600

    Add files via upload

--- ejemplo2.kt
+++ ejemplo2.kt
@@ -1,3 +1,6 @@
+/*La clase Dado define un método tirar que almacena en la propiedad
+valor un número aleatorio comprendido entre 1 y 6.
+Además llama al método imprimir para mostrarlo:*/
 class Dado (var valor: Int){
     fun tirar() {
         valor = ((Math.random() * 6) + 1).toInt()
@@ -7,10 +10,13 @@ class Dado (var valor: Int){
         println("Valor del dado: $valor")
     }
 }
+/*La clase JuegoDeDados define tres propiedades de la clase Dado:*/
 class JuegoDeDados {
     val dado1 = Dado(1)
     val dado2 = Dado(1)
     val dado3 = Dado(1)
+    /*En el método jugar de la clase JuegoDeDados procedemos a pedir a cada dado que se tire y verificamos si los tres valores
+son iguales:*/
     fun jugar() {
         dado1.tirar()
         dado2.tirar()
@@ -21,6 +27,7 @@ class JuegoDeDados {
             print("Perdió")
     }
 }
+/*En la función main de nuestro programa creamos un objeto de la clase JuegoDeDados:*/
 fun main(parametro: Array<String>) {
     val juego1 = JuegoDeDados()
     juego1.jugar()
