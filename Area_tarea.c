6b1a995deaf80d9a6b5e6fec49b763372024e59b
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-26 18:41:41 -0600

    Add files via upload

---
+++ Area_tarea.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main (){
+	
+	double d=10;
+	printf("ingrese el diametro\n",d);
+	
+	double r = d/2 ;
+	
+	printf("el radio es\n",r);
+	double pi = 3.1416;
+	double area = pi * r * r;
+	printf(" el area es: %f ", area);
+	printf("\n");
+	printf("Pause");
+
+return 0;
+
+	
+	
+	
+	
+	
+}
