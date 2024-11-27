f8a40e9a491cc5196623861063490c4f51cc85ae
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-26 18:42:45 -0600

    Add files via upload

---
+++ Bienvenida.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf( " BIENVENIDO \n" );
+    printf( " ES UN GUSTO COMPILAR PARA ADRIANA " );
+    printf( " \n" );
+    system("pause");
+    
+    return 0;
+}
