be1530dae686e21a2430f38b7ef11da3ce24c10f
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-27 20:20:02 -0600

    Add files via upload

---
+++ Practica_11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define PI 3.141592654
+int main()
+{
+    int x=5;
+    int y=10;
+    int z=++x*y--;
+    printf("x : %d\n",x);
+    printf("y : %d\n",y);
+    printf("z : %d\n",z);
+    
+
+    printf("pause");
+    return 0;
+    
+}
