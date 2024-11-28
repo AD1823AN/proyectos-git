36886563283790544b7f404c385c6f1df080ca1b
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-27 20:19:00 -0600

    Add files via upload

---
+++ Practica_9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define PI 3.141592654
+int main()
+{
+    int c=5;
+    printf("c : %d\n",c);
+    printf("c++ : %d\n",c++);
+    printf("c:%d\n",c);
+    
+    int d=10;
+    printf("d: %d\n",d);
+    printf("d: %d\n",--d);
+    printf("d: %d\n",d);
+    printf("pause");
+    return 0;
+    
+}
