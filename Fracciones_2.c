d7fcaae1f17fc6cf0c6b5ab2b102c4a04ddfd168
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-27 20:16:44 -0600

    Add files via upload

---
+++ Fracciones_2.c
@@ -0,0 +1,16 @@
+
+# include <stdio.h>
+
+int main (){
+	double a=1;
+	double b=3;
+	double c=5;
+	double d=23;
+	double e=30;
+	
+	printf("El Resultado es de es: %f\n",(a/b+b/c+((a/e)/(d/e))));
+	system("pause");
+	return 0;
+}
+
+
