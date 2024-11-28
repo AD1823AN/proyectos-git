71907ddc3ff16c6cb38db7a8aabd439059bd7207
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-27 20:15:56 -0600

    Add files via upload

---
+++ Fraccion1.c
@@ -0,0 +1,13 @@
+
+# include <stdio.h>
+
+int main (){
+	double a=1;
+	double b=2;
+	double c=4;
+	double d=5;
+	
+	printf("El Resultado es de es: %f\n",(d+(b/(a/(a+(b/(b-(a/c))))))));
+	system("pause");
+	return 0;
+}
