82516ef85082ff72393ebbfc06324b96522292b4
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-28 19:58:04 -0600

    Add files via upload

---
+++ Practica1.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf( " ESTE ES UN TEXTO \n" );
+    printf( " QUE FUE IMPRESO DESDE C" );
+    printf( " \n" );
+    printf( " YA SOY TODO UN PROGRAMADOR  \n" );
+    printf( "  \n" );
+    system("pause");
+    
+    return 0;
+}
