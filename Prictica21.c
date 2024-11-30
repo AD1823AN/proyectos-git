29629e587325be8af6a8e95d6442ca6ccc213a5f
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-29 21:21:57 -0600

    Add files via upload

---
+++ Prictica21.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main (){
+int	x1;
+int x2;
+int x=1;
+while(x<500){
+	x1=(x*x);
+	x2=(x*x*x);
+	printf("%d,%d",x1,x2);
+	printf("\n");
+    x++;
+	
+}
+
+
+		printf("pause ");
+		return 0;
+}
