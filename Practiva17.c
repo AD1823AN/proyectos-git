bfa820429bed23fd449ac777e94726016fe802c6
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-29 21:22:49 -0600

    Add files via upload

---
+++ Practiva17.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main (){
+int numero =98;
+if(numero<50);
+printf("EL NUMERO ES MENOR QUE 50");
+else 
+
+printf (" EL NUMERO ES MAYOR QUE 50");
+printf("fin del prograna\n");
+
+	
+	return 0;
+
+}
+
