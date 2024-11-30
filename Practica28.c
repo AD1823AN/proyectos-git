8dc63d61f5725d3d429f7b99d382bdb0075a842c
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-29 21:24:37 -0600

    Add files via upload

---
+++ Practica28.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+int main (){
+int a[10] = [3,5,6,8,4,7,8,5,3,1];
+int b[10] = [3,4,6,8,9,1,2,3,0,9];
+
+
+
+printf("%d\n",a[3]mod(b[2]/2));
+printf("%d\n",b[a[1]]-a[9]);
+printf("%d\n",b[0]+a[1+2]);
+printf("%d\n",a[5]+b[5]);
+printf("%d\n",(a[3]/b[2])/2);
+
+return 0;
+}
+
+
+
+
