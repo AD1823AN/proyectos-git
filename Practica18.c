ac91ef9181ce775a0388e2ee7a42a8c825f2a08b
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-30 20:27:04 -0600

    Add files via upload

---
+++ Practica18.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main (){
+	
+int hts=40;
+int ph;
+int sm;
+
+if(hts<=40) {
+sm=hts*40;
+printf("el salario es ",sm);
+}else (hts>=40){
+	sm=40*ph+(ph*1.5*(hts-40));
+printf("tu salirio es ",sm);
+		printf("pause");
+}
+	return 0;
+}
+
