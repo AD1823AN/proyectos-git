28b9d48bdf70425fd2f2b034c05ad33c2388cba4
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-11-30 20:22:55 -0600

    Add files via upload

---
+++ Practica21.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+int main (void){
+	
+int i,aux,numero,iterar,divisor=0;
+
+printf(" ingrese el numero ");
+scanf("%d",&numero);
+printf("\nDivisores:\n");
+if(numero %2==0){
+	iterar=numero/2;
+} else {
+	iterar=(numero-1)/2;
+}
+for(i=1<=iterar,1++){
+	if(numero%i==0){
+		aux=numero/i
+	}
+}
+
+
+
+
+printf("pause")
+
+return 0;
+//while(a<=numero>=1000){
+//resultado=div(numero,a);
+//printf("%d,",resultado);
+//a++;
+}
+	
+
+
+
