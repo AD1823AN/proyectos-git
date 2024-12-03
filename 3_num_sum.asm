124fa705f7d2e59644cc1b6eb124f6086ba01c23
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2024-12-02 22:46:16 -0600

    Update 3_num_sum.asm

--- 3_num_sum.asm
+++ 3_num_sum.asm
@@ -10,7 +10,6 @@ msg db 10,13,"!! Ingrese un numero de 3 digitos !!$"
 msg1 db 10,13,"!! Numero ingresado: !!$"
 msg2 db 10,13,"!! La suma de los numeros es: !!$"
 msg3 db 10,13,"!! El mayor de los digitos es: !!$"
-
 .code
 mov ax, @data
 mov ds, ax
